8453bc6eab84b3162f8f4ae57b506db56d6dd386
Paul Grant <paul_grant@med.unc.edu>
2020-11-23 08:15:00 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   01_deck.ipynb
    	modified:   deck_of_cards/_nbdev.py
    	new file:   deck_of_cards/deck.py

--- deck_of_cards/_nbdev.py
+++ deck_of_cards/_nbdev.py
@@ -4,7 +4,8 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"Card": "00_card.ipynb"}
 
-modules = ["card.py"]
+modules = ["card.py",
+           "deck.py"]
 
 doc_url = "https://poglesbyg.github.io/deck_of_cards/"
 
